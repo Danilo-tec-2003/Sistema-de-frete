781f3e940494c655ef6ab57b6234749f7b781638
Danilo Mendes <danilodev.br@gmail.com>
2026-04-20 16:45:18 -0300

    feat(veiculo): model, DAO, BO e Controlador

---
+++ src/main/java/br/com/gw/veiculos/Veiculo.java
@@ -0,0 +1,9 @@
+// Veículos
+//  Placa (formato Mercosul ou antigo), RNTRC, ano de fabricação
+//  Tipo: Truck / Carreta / Van / Utilitário
+//  Tara (kg), capacidade de carga (kg), volume (m3)
+//  Status: Disponível / Em Viagem / Em Manutenção
+
+public class Veiculo {
+    
+}
