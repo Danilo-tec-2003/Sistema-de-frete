74a1ef66d6b6ff37bc9f5045de11daa224e7abf6
Danilo Mendes <danilodev.br@gmail.com>
2026-04-20 16:44:41 -0300

    feat(cliente): model, DAO, BO e Controlador

---
+++ src/main/java/br/com/gw/cliente/Cliente.java
@@ -0,0 +1,11 @@
+// Clientes (Tomadores de Serviço)
+//  Razão social, nome fantasia, CNPJ, inscrição estadual, tipo (Remetente /
+// Destinatário / Ambos)
+//  Endereço completo: logradouro, número, complemento, bairro, município, UF, CEP
+//  Contato: telefone, e-mail
+// Status: Ativo / Inativo
+
+
+public class Cliente {
+    
+}
