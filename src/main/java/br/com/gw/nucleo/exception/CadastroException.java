d987d0ef99f2bfa451f74d38b0fa66a2a098a445
Danilo Mendes <danilodev.br@gmail.com>
2026-04-21 16:23:53 -0300

    feat(exception): atualiza hierarquia NegocioException e CadastroException

--- src/main/java/br/com/gw/nucleo/exception/CadastroException.java
+++ src/main/java/br/com/gw/nucleo/exception/CadastroException.java
@@ -1,3 +1,15 @@
-public class CadastroException {
-    
+package br.com.gw.nucleo.exception;
+
+/** Violações em cadastros: CPF duplicado, CNPJ inválido, campos obrigatórios, etc. */
+public class CadastroException extends NegocioException {
+
+    private static final long serialVersionUID = 1L;    
+
+    public CadastroException(String mensagem) {
+        super(mensagem);
+    }
+
+    public CadastroException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
+    }
 }
